53c2353ec2733adf655cc5d4034884e817a20e68
Patrick <da.heuschkel@gmail.com>
2023-02-26 16:12:39 +0100

    More cleanup, unify credentials with loginstatus

--- app/src/main/java/com/elliecoding/onetapdrive/MainActivity.kt
+++ app/src/main/java/com/elliecoding/onetapdrive/MainActivity.kt
@@ -18,10 +18,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Scope
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.services.drive.DriveScopes
-import java.util.Collections
 
 private const val REQUEST_CODE_ONE_TAP = 0
 private const val REQUEST_CODE_LEGACY = 1
@@ -43,19 +41,19 @@ class MainActivity : AppCompatActivity(), UserViewModel.UserEventCallback {
         binding.inputButton.setOnClickListener { storeText(binding.inputData.text.toString()) }
         binding.startLegacy.setOnClickListener { startLegacySignIn() }
         binding.startOnetap.setOnClickListener { startOneTapSignIn() }
-        userViewModel.userLoginStatus.observe(this) { isLoggedIn ->
+        binding.signOut.setOnClickListener { signOut() }
+        userViewModel.userLogin.observe(this) { status ->
+            val isLoggedIn = status.first != null
             binding.textLogin.text = if (isLoggedIn) "Logged in" else "Not logged in"
+            binding.textLoginError.text = status.second?.localizedMessage
             binding.startOnetap.isEnabled = !isLoggedIn
             binding.startLegacy.isEnabled = !isLoggedIn
+            binding.signOut.isEnabled = isLoggedIn
             binding.inputData.isEnabled = isLoggedIn
             binding.inputButton.isEnabled = isLoggedIn
             binding.delete.isEnabled = isLoggedIn
             if (isLoggedIn) {
-                try {
-                    userViewModel.download(this)
-                } catch (e: UserRecoverableAuthIOException) {
-                    startActivityForResult(e.intent, 9);
-                }
+                userViewModel.download(this)
             }
         }
 
@@ -67,6 +65,11 @@ class MainActivity : AppCompatActivity(), UserViewModel.UserEventCallback {
         prepareLegacySignIn()
     }
 
+    private fun signOut() {
+        userViewModel.saveLoginFailure(null)
+        oneTapClient.signOut()
+    }
+
     private fun startLegacySignIn() {
         startActivityForResult(legacyClient.signInIntent, REQUEST_CODE_LEGACY)
     }
@@ -76,96 +79,31 @@ class MainActivity : AppCompatActivity(), UserViewModel.UserEventCallback {
         userViewModel.upload(this, text)
     }
 
-    private fun processLegacySignIn(data: Intent?) {
-        if (data == null) return
-        GoogleSignIn.getSignedInAccountFromIntent(data)
-            .addOnSuccessListener { googleAccount: GoogleSignInAccount ->
-                Log.d(TAG, "Signed in as " + googleAccount.email)
-                makeDriveCredential(googleAccount)
-                userViewModel.userLoginStatus.value = true
-
-            }
-            .addOnFailureListener { exception: Exception? ->
-                Log.e(TAG, "Unable to sign in.", exception)
-                userViewModel.userLoginStatus.value = false
-            }
-    }
-
-    private fun makeDriveCredential(googleAccount: GoogleSignInAccount) {
-        // Use the authenticated account to sign in to the Drive service.
-        val credential: GoogleAccountCredential = GoogleAccountCredential.usingOAuth2(
-            this, Collections.singleton(DriveScopes.DRIVE_APPDATA)
-        )
-        credential.selectedAccount = googleAccount.account
-        userViewModel.credentials = credential
-    }
-
-    private fun makeDriveCredential(oneTapSignIn: SignInCredential) {
-        // Use the authenticated account to sign in to the Drive service.
-        val credential: GoogleAccountCredential = GoogleAccountCredential.usingOAuth2(
-            this, Collections.singleton(DriveScopes.DRIVE_APPDATA)
-        )
-        credential.selectedAccount = Account(oneTapSignIn.id, packageName)
-        userViewModel.credentials = credential
-    }
-
-    private fun processOneTapSignIn(data: Intent) {
-        try {
-            val oneTapCredential: SignInCredential =
-                oneTapClient.getSignInCredentialFromIntent(data)
-            Log.d(TAG, "Signed in as " + oneTapCredential.displayName)
-            makeDriveCredential(oneTapCredential)
-            userViewModel.userLoginStatus.value = true
-        } catch (e: ApiException) {
-            Log.e(TAG, "Credentials API error", e)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            REQUEST_CODE_ONE_TAP -> {
-                processOneTapSignIn(data!!)
-            }
-
-            REQUEST_CODE_LEGACY -> {
-                processLegacySignIn(data)
-            }
-
-            9 -> {
-                Log.d(TAG, resultCode.toString())
-            }
-        }
-    }
-
     private fun startOneTapSignIn() {
         oneTapClient.beginSignIn(signInRequest).addOnSuccessListener(this) { result ->
+            // signInLauncher.launch(
+            // IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+            //)
             try {
-                // signInLauncher.launch(
-                // IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
-                //)
-                try {
-                    startIntentSenderForResult(
-                        result.pendingIntent.intentSender, REQUEST_CODE_ONE_TAP,
-                        null, 0, 0, 0, null
-                    )
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-                Log.d(TAG, "SignIn started")
+                startIntentSenderForResult(
+                    result.pendingIntent.intentSender, REQUEST_CODE_ONE_TAP,
+                    null, 0, 0, 0, null
+                )
             } catch (e: IntentSender.SendIntentException) {
                 Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                userViewModel.saveLoginFailure(e)
             }
+            Log.d(TAG, "SignIn started")
         }.addOnFailureListener(this) { e ->
             // No saved credentials found. Launch the One Tap sign-up flow, or
             // do nothing and continue presenting the signed-out UI.
             Log.d(TAG, "SignIn failed", e)
-            userViewModel.userLoginStatus.value = false
+            userViewModel.saveLoginFailure(e)
         }
     }
 
     private fun prepareLegacySignIn() {
-        Log.d(TAG, "Requesting sign-in")
+        Log.d(TAG, "Requesting legacy SignIn")
         val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestScopes(Scope(DriveScopes.DRIVE_APPDATA))
@@ -181,13 +119,58 @@ class MainActivity : AppCompatActivity(), UserViewModel.UserEventCallback {
                     .setSupported(true)
                     // Your server's client ID, not your Android client ID.
                     .setServerClientId(getString(R.string.web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
+                    .setFilterByAuthorizedAccounts(false)
                     .build()
             )
             // Automatically sign in when exactly one credential is retrieved.
             .setAutoSelectEnabled(true)
             .build()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_ONE_TAP -> {
+                processOneTapSignIn(data)
+            }
+
+            REQUEST_CODE_LEGACY -> {
+                processLegacySignIn(data)
+            }
+
+            9 -> {
+                Log.d(TAG, resultCode.toString())
+            }
+        }
+    }
+
+    private fun processLegacySignIn(data: Intent?) {
+        if (data == null) return
+        GoogleSignIn.getSignedInAccountFromIntent(data)
+            .addOnSuccessListener { googleAccount: GoogleSignInAccount ->
+                Log.d(TAG, "Signed in as " + googleAccount.email)
+                if (googleAccount.account == null) {
+                    userViewModel.saveLoginFailure(null)
+                } else {
+                    userViewModel.saveLoginSuccess(this, googleAccount.account!!)
+                }
+            }
+            .addOnFailureListener { exception: Exception? ->
+                Log.e(TAG, "Unable to sign in.", exception)
+                userViewModel.saveLoginFailure(exception)
+            }
+    }
+
+    private fun processOneTapSignIn(data: Intent?) {
+        try {
+            val oneTapCredential: SignInCredential =
+                oneTapClient.getSignInCredentialFromIntent(data)
+            Log.d(TAG, "Signed in as " + oneTapCredential.displayName)
+            userViewModel.saveLoginSuccess(this, Account(oneTapCredential.id, packageName))
+        } catch (e: ApiException) {
+            Log.e(TAG, "Credentials API error", e)
+            userViewModel.saveLoginFailure(e)
+        }
     }
 
     override fun onDownloadError(cause: Throwable) {
--- app/src/main/java/com/elliecoding/onetapdrive/UserViewModel.kt
+++ app/src/main/java/com/elliecoding/onetapdrive/UserViewModel.kt
@@ -1,15 +1,17 @@
 package com.elliecoding.onetapdrive
 
+import android.accounts.Account
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.api.client.http.HttpRequestInitializer
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.services.drive.DriveScopes
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
-import java.util.Calendar
-import java.util.Date
+import java.lang.Exception
+import java.util.Collections
 
 private const val TAG = "UserViewModel"
 
@@ -24,14 +26,28 @@ class UserViewModel : ViewModel() {
         println("CoroutineExceptionHandler got $exception")
         downloadCallback?.onDownloadError(exception)
     }
-    var credentials: HttpRequestInitializer? = null
-    val userLoginStatus = MutableLiveData(false)
+
+    // Pair(Account, Error)
+    val userLogin = MutableLiveData(Pair<GoogleAccountCredential?, Exception?>(null, null))
     val storedData = MutableLiveData<String?>()
+
+    fun saveLoginSuccess(context: Context, account: Account) {
+        // Use the authenticated account to sign in to the Drive service.
+        userLogin.value = Pair(
+            GoogleAccountCredential.usingOAuth2(
+                context, Collections.singleton(DriveScopes.DRIVE_APPDATA)
+            ).setSelectedAccount(account), null
+        )
+    }
+
+    fun saveLoginFailure(exception: Exception?) {
+        userLogin.value = Pair(null, exception)
+    }
 
     fun download(userEventCallback: UserEventCallback) {
         downloadCallback = userEventCallback
         viewModelScope.launch(downloadExceptionHandler) {
-            val result = DriveStorage.download(credentials!!)
+            val result = DriveStorage.download(userLogin.value!!.first!!)
             Log.i(TAG, "download: $result")
             storedData.value = result
         }
@@ -39,12 +55,14 @@ class UserViewModel : ViewModel() {
 
     fun upload(context: Context, data: String) {
         viewModelScope.launch {
-            DriveStorage.upload(context, credentials!!, data)
+            DriveStorage.upload(context, userLogin.value!!.first!!, data)
         }
     }
 
     fun delete(context: Context) {
         storedData.value = ""
-        viewModelScope.launch { DriveStorage.delete(context, credentials!!) }
+        userLogin.value?.first?.let {
+            viewModelScope.launch { DriveStorage.delete(context, it) }
+        }
     }
 }
